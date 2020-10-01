e1129064769d8d60ed8132e30c0cecefd6da02f1
Mark Slater <mark.slater@manicphotons.com>
2020-10-01 14:06:19 +0100

    Respond to close requests from WM

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*─────────────────────────────────────────────────────────────────────────────
+ * PENDING TASKS
+ *
+ * Check for and correctly handle errors when using xlib.
+ * Set all relavent window properties (WM_NAME etc…) for window manager usage.
+ * Load/save settings from/to a config file.
+ * Create vulkan interface.
+ * Handle window resizing.
+ * Enable both fullscreen and windowed modes.
+ */
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xatom.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define IGNORE_INPUT( Arg ) ((void)Arg);
 
@@ -16,10 +26,6 @@ main( int ArgCount, char** ArgValues ){
 	IGNORE_INPUT( ArgCount  );
 	IGNORE_INPUT( ArgValues );
 
-	int32_t ScreenBitDepth = 24;
-	int32_t WindowPosition[2] = {0, 0};
-	int32_t WindowDimensions[2] = {800, 600};
-
 	Display* DefaultDisplay = XOpenDisplay( NULL );
 	if( !DefaultDisplay ){
 		PrintError( "Xlib: Could not open the default display." );
@@ -29,12 +35,15 @@ main( int ArgCount, char** ArgValues ){
 	int RootWindow = DefaultRootWindow( DefaultDisplay );
 	int DefaultScreen = DefaultScreen( DefaultDisplay );
 
+	int32_t ScreenBitDepth = 24;
 	XVisualInfo VisualInfo = {};
 	if( !XMatchVisualInfo( DefaultDisplay, DefaultScreen, ScreenBitDepth, TrueColor, &VisualInfo) ){
 		PrintError( "Xlib: No matching visual info." );
 		return EXIT_FAILURE;
 	}
 
+	int32_t WindowPosition[2] = {0, 0};
+	int32_t WindowDimensions[2] = {800, 600};
 	XSetWindowAttributes WindowAttributes;
 	WindowAttributes.background_pixel = 0;
 	WindowAttributes.colormap = XCreateColormap( DefaultDisplay, RootWindow, VisualInfo.visual, AllocNone );
@@ -54,6 +63,13 @@ main( int ArgCount, char** ArgValues ){
 	}
 
 	XStoreName( DefaultDisplay, WindowID, "Hello, World!" );
+
+	Atom DeleteWindowProtocol = XInternAtom( DefaultDisplay, "WM_DELETE_WINDOW", False);
+	if( !XSetWMProtocols( DefaultDisplay, WindowID, &DeleteWindowProtocol, 1 ) ){
+		PrintError( "Xlib: Delete window protocol wasn't set properly" );
+		return EXIT_FAILURE;
+	}
+
 	XMapWindow( DefaultDisplay, WindowID );
 	XFlush( DefaultDisplay );
 
@@ -63,6 +79,11 @@ main( int ArgCount, char** ArgValues ){
 		while( XPending( DefaultDisplay ) > 0 ){
 			XNextEvent( DefaultDisplay, &Event );
 			switch( Event.type ){
+				case ClientMessage: {
+					if( Event.xclient.data.l[0] == DeleteWindowProtocol ) {
+						XDestroyWindow( DefaultDisplay, WindowID );
+					}
+				}
 				case DestroyNotify: {
 					XDestroyWindowEvent* e = (XDestroyWindowEvent*)&Event;
 					if( e->window == WindowID ){
@@ -72,6 +93,9 @@ main( int ArgCount, char** ArgValues ){
         		}
 		}
 	};
+
+	XCloseDisplay( DefaultDisplay );
+	DefaultDisplay = NULL;
 
 	return EXIT_SUCCESS;
 }
